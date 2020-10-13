4ce954033c2f041332c45405d4c75c4b18f90feb
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-10-13 11:38:00 -0700

    Update inspect.tf

--- inspect.tf
+++ inspect.tf
@@ -76,10 +76,11 @@ resource "aws_inspector_assessment_template" "bar-template" {
   target_arn = aws_inspector_assessment_target.myinspect.arn
   duration   = 180
   rules_package_arns = [
-     "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-rExsr2X8",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gBONHN9h",
+    "arn:aws:inspector:us-east-2:646659390643:rulespackage/0-JnA8Zp85",
+    "arn:aws:inspector:us-east-2:646659390643:rulespackage/0-m8r61nnh",
+    "arn:aws:inspector:us-east-2:646659390643:rulespackage/0-cE4kTR30",
+    "arn:aws:inspector:us-east-2:646659390643:rulespackage/0-AxKmMHPX",
+
   ]
 }
 
